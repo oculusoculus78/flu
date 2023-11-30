42fdd9994369d59b3b12862749f6b1cb668981d6
oculusoculus78 <148907170+oculusoculus78@users.noreply.github.com>
2023-11-30 03:41:16 -0500

    Update fluky-installation.sh

--- fluky-installation.sh
+++ fluky-installation.sh
@@ -51,11 +51,11 @@ function ubuntu_18_04 {
 
     echo "$upgrade_system"
 
-    # install php7.3
+    # install php7.4
     apt install software-properties-common -y
 
     add-apt-repository ppa:ondrej/php -y
-    apt install php7.3 php7.3-mbstring php7.3-mysqli php7.3-curl php7.3-dom php7.3-xml php7.3-xmlwriter php7.3-common php7.3-json php7.3-zip php7.3-bcmath php7.3-gettext -y
+    apt install php7.4 php7.4-mbstring php7.4-mysqli php7.4-curl php7.4-dom php7.4-xml php7.4-xmlwriter php7.4-common php7.4-json php7.4-zip php7.4-bcmath php7.4-gettext -y
 
     # install apache with certbot
     apt install python-certbot-apache -y
